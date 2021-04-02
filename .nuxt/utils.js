a9053957ed8df157376c70231a7a4ee12b82d2eb
Sachin <sachinghuge865@gmail.com>
2021-04-02 16:49:04 +0530

    UI

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -169,7 +169,7 @@ export async function setContext (app, context) {
       payload: context.payload,
       error: context.error,
       base: '/',
-      env: {"razorPayKey":"rzp_live_m6AS1DmObyiIoj"}
+      env: {}
     }
     // Only set once
     if (!process.static && context.req) {
